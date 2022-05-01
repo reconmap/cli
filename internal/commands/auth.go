fa443f04dbce8b7ecc0847db14bb76134f6cc539
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2022-05-01 19:43:47 +0100

    Tidy up login/logout code

--- internal/commands/auth.go
+++ internal/commands/auth.go
@@ -1,14 +1,14 @@
 package commands
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/reconmap/cli/internal/api"
 	"github.com/reconmap/cli/internal/configuration"
@@ -23,14 +23,15 @@ func Login(username string, password string) error {
 	}
 	var apiUrl string = config.ApiUrl + "/users/login"
 
-	formData := url.Values{
-		"username": {username},
-		"password": {password},
+	formData := map[string]string{
+		"username": username,
+		"password": password,
 	}
+	jsonData, err := json.Marshal(formData)
 
 	client := &http.Client{}
-	req, err := httputils.NewRmapRequest("POST", apiUrl, strings.NewReader(formData.Encode()))
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req, err := httputils.NewRmapRequest("POST", apiUrl, bytes.NewBuffer(jsonData))
+	req.Header.Add("Content-Type", "application/json")
 	response, err := client.Do(req)
 	if err != nil {
 		return err
@@ -72,21 +73,22 @@ func Login(username string, password string) error {
 }
 
 func Logout() error {
+	if _, err := httputils.ReadSessionToken(); err != nil {
+		return errors.New("There is no active user session")
+	}
+
 	config, err := configuration.ReadConfig()
 	if err != nil {
 		return err
 	}
 	var apiUrl string = config.ApiUrl + "/users/logout"
 
-	formData := url.Values{}
-
 	client := &http.Client{}
-	req, err := httputils.NewRmapRequest("POST", apiUrl, strings.NewReader(formData.Encode()))
+	req, err := httputils.NewRmapRequest("POST", apiUrl, nil)
 	if err != nil {
 		return err
 	}
 
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	if err = httputils.AddBearerToken(req); err != nil {
 		return err
 	}
@@ -97,34 +99,23 @@ func Logout() error {
 	}
 
 	if response.StatusCode != http.StatusOK {
-
-		if response.StatusCode == http.StatusForbidden || response.StatusCode == http.StatusUnauthorized {
-			return errors.New("Invalid credentials")
-		}
-
 		return errors.New("Response error received from the server")
 	}
 
 	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		return errors.New("Unable to read response from server")
-	}
-
-	var loginResponse api.LoginResponse
-
-	if err = json.Unmarshal([]byte(body), &loginResponse); err != nil {
-		return err
-	}
 
 	configPath, err := httputils.GetSessionTokenPath()
-
-	err = os.Remove(configPath)
-	if err == nil {
-		terminal.PrintGreenTick()
-		fmt.Printf(" Successfully logged out from the server\n")
+	if _, err := os.Stat(configPath); err == nil {
+		err = os.Remove(configPath)
+		if err != nil {
+			log.Println("Unable to remove file")
+		}
+	} else if errors.Is(err, os.ErrNotExist) {
+		log.Println("warning: Session file does not exist")
 	}
+
+	terminal.PrintGreenTick()
+	fmt.Printf(" Successfully logged out from the server\n")
 
 	return err
 }
